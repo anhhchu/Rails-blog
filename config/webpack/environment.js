14b572d933744dc35458f6d21babec4781d06da6
Ann <anhchu1291@gmail.com>
2022-01-22 11:36:13 -0600

    Rails blog with bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,17 @@
+// Reference: https://www.mashrurhossain.com/blog/rails6bootstrap4
+
 const { environment } = require('@rails/webpacker')
+
+const webpack = require("webpack")
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+
+$: 'jquery',
+
+jQuery: 'jquery',
+
+Popper: ['popper.js', 'default']
+
+}))
 
 module.exports = environment
